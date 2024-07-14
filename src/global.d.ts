c2109dde962b9e5971f72f4078d7c39900357fb3
wrule <1982775886@qq.com>
2024-07-14 14:59:57 +0800

    xjson_de

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,7 @@ declare global {
     decycle(value: any, replacer?: (value: any) => any): any;
     retrocycle(value: any): any;
     xjson(value: any): any;
+    xjson_de(value: any): any;
     xstringify(...args: Parameters<typeof JSON.stringify>);
     xparse(...args: Parameters<typeof JSON.parse>);
   }
