230f3faf688c41b29c50c19a17fa70e6bcbe90b3
wrule <1982775886@qq.com>
2024-07-14 22:09:50 +0800

    删除types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import './JSON-js/cycle_ext';
 import { mapping_forward, mapping_reverse, traverse, xjson_decycle } from './mapping';
 
+declare global {
+  interface JSON {
+    decycle(value: any, replacer?: (value: any) => any): any;
+    retrocycle(value: any): any;
+    xjson(object: any, replacer?: (value: any) => any): any;
+    xjson_de(object: any, replacer?: (value: any) => any): any;
+    xstringify(...args: Parameters<typeof JSON.stringify>): ReturnType<typeof JSON.stringify>;
+    xparse(...args: Parameters<typeof JSON.parse>): ReturnType<typeof JSON.parse>;
+  }
+}
+
 JSON.xjson = (object: any, replacer?: (value: any) => any) => {
   return JSON.decycle(object, (value) => {
     const result = mapping_forward(value);
