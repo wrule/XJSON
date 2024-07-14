c8d99172917e6040b710ea9a39f81108a92540cf
wrule <1982775886@qq.com>
2024-07-14 14:05:10 +0800

    _JSON

--- src/JSON-js/cycle_ext.ts
+++ src/JSON-js/cycle_ext.ts
@@ -1,23 +1,25 @@
 import './cycle.js';
 import { xjson_decycle } from '../index';
 
-JSON._decycle = JSON.decycle;
+const _JSON: any = JSON;
+
+_JSON._decycle = JSON.decycle;
 JSON.decycle = (value: any) => {
   const protoType = Object.prototype.toString.call(value);
   if (protoType === '[object Object]' || protoType === '[object Array]') {
-    const result = JSON._decycle(value);
+    const result = _JSON._decycle(value);
     result[xjson_decycle] = true;
     return result;
   }
   return value;
 };
 
-JSON._retrocycle = JSON.retrocycle;
+_JSON._retrocycle = JSON.retrocycle;
 JSON.retrocycle = (value: any) => {
   const protoType = Object.prototype.toString.call(value);
   if (protoType === '[object Object]' || protoType === '[object Array]') {
     if (!value[xjson_decycle]) return value;
-    const result = JSON._retrocycle(value);
+    const result = _JSON._retrocycle(value);
     delete result[xjson_decycle];
     return result;
   }
--- src/global.d.ts
+++ src/global.d.ts
@@ -3,8 +3,6 @@ declare global {
   interface JSON {
     decycle(value: any): any;
     retrocycle(value: any): any;
-    _decycle(value: any): any;
-    _retrocycle(value: any): any;
   }
 }
 
